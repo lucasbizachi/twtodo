05cd9e6c77c5f80270c6d1adca9ffd9c0876c37e
lucasbizachi <bizachilucas@gmail.com>
2024-01-18 14:02:02 -0300

    criando rotas

--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import Todo
 
 def todo_list(request):
-    return render(request, "todos/todo_list.html")
+    todos= Todo.objects.all()
+    return render(request, "todos/todo_list.html", {"todos": todos})
 
 # Create your views here.
